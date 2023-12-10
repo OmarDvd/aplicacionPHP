d3f1a423f20756c40b1fb3e77e7e4d2859d2a362
Omar <omarescamez@gmail.com>
2023-12-10 10:58:48 +0100

    subida app

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="#" method="POST">
+        Numero de cuenta <br>
+        <input type="text" id="cuenta" name="cuenta"><br>
+        Nombre <br>
+        <input type="text" id="nombre" name="nombre"><br>
+        <br>
+        <input type="submit" id="enviar" name="enviar" value="enviar"><br>
+    </form>
+</body>
+</html>
